8d571676a535b9219f69127ac5e56084412df7ed
Joyce Lopes Krüger <homeofficina@gmail.com>
2023-06-11 22:13:23 -0300

    resolvi o exercicio

--- src/Components/PokemonCard/PokemonCard.js
+++ src/Components/PokemonCard/PokemonCard.js
@@ -1,20 +1,37 @@
 import React from 'react'
-import {Card, PokemonName, PokemonType, EvolveButton} from './styles'
+import {
+  Card, 
+  PokemonName, 
+  PokemonType, 
+  EvolveButton
+} from './styles'
 
 
 
-const PokemonCard = (props) => {
+const PokemonCard = ({
+  pokemon,
+  setPokemon,
+  evolucao,
+  evolucao2
+}) => {
 
     const evoluirPokemon = () => {
+      if (!pokemon.evolved && evolucao) {
+        setPokemon(evolucao);
+      } else if (pokemon.evolved && evolucao2) {
+        setPokemon(evolucao2);
+      } else {
+        alert("Este pokemon não tem mais evoluções");
+      }
         console.log("Cliquei no botão de evoluir")
     }
     
   return (
-    <Card color={}>
-        <img src={} alt={`Pokemon`}/>
-        <PokemonName>{}</PokemonName>
-        <PokemonType>{}</PokemonType>
-        <p>{}kg</p>
+    <Card color={pokemon.color}>
+        <img src={pokemon.img} alt={`Pokemon`}/>
+        <PokemonName>{pokemon.name}</PokemonName>
+        <PokemonType>{pokemon.type}</PokemonType>
+        <p>{pokemon.weith}kg</p>
 
         <EvolveButton onClick={() => evoluirPokemon()}>Evoluir!</EvolveButton>
     </Card>
